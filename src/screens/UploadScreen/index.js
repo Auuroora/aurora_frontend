99b01abbdd657d509314542f2d06428100695b52
Jihwan Kim <noneobj@gmail.com>
2020-04-06 11:15:41 +0900

    Fix eslint problem

--- src/screens/UploadScreen/index.js
+++ src/screens/UploadScreen/index.js
@@ -1,29 +1,30 @@
-import React, {Component} from 'react';
+/* eslint-disable react/prop-types */
+import React, {Component} from 'react'
 import {
-    View,
-    Text,
-    Button,
-    StyleSheet
-} from 'react-native';
+  View,
+  Text,
+  Button,
+  StyleSheet
+} from 'react-native'
 export default class UploadScreen extends Component{
 
-    render(){
-        return (
-            <View style={styles.container}>
-                <Text>UploadScreen</Text>
-                <Button
-                    title='back'
-                    onPress={() => this.props.navigation.goBack()} />
-            </View>
-        );
-    }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>UploadScreen</Text>
+        <Button
+          title='back'
+          onPress={() => this.props.navigation.goBack()} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })

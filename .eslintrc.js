3116ddfbdddf3ed311d7afce8355b78d5a7187ee
Jihwan Kim <noneobj@gmail.com>
2020-04-06 09:39:49 +0900

    Add eslint file and ignore, install packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser" : "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-native"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "keyword-spacing": 0,
+        "no-unused-vars": 1,
+        "no-multiple-empty-lines": 0,
+        "space-before-function-paren": 0,
+        "eol-last": 0
+    }
 };

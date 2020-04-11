e4b8fedad6c19a1e928a67ff213690c4651a9da4
zzudy <wngus1100@gmail.com>
2020-04-11 22:08:54 +0900

    homescreen 작성

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,25 +2,36 @@ import React, {Component} from 'react'
 import {
   View,
   ScrollView,
-  StyleSheet
+  StyleSheet,
 } from 'react-native'
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen'
+
+import CardComponent from '../../component/Card'
 
 
 export default class HomeScreen extends Component{
   render(){
     return (
+      //무한 스크롤 적용해야함 
       <ScrollView style={styles.container}>
-        <View style={styles.wrapContent}>
-          <View style={styles.content}></View>
+        <View style={{flexDirection:"row"}}>
+          <View style={styles.wrapContent}>
+            <CardComponent imageSource='1' likes='12'/>
+          </View>   
+          <View style={styles.wrapContent}>
+            <CardComponent imageSource='2' likes='11'/>
+          </View>   
         </View>
-        <View style={styles.wrapContent}>
-          <View style={styles.content}></View>
+        <View style={{flexDirection:"row"}}>
+          <View style={styles.wrapContent}>
+            <CardComponent imageSource='1' likes='22'/>
+          </View>   
+          <View style={styles.wrapContent}>
+            <CardComponent imageSource='2' likes='11'/>
+          </View>    
         </View>
-        <View style={styles.wrapContent}>
-          <View style={styles.content}></View>
-        </View>
-      </ScrollView>
+
+      </ScrollView>       
     )
   }
 }
@@ -28,12 +39,14 @@ export default class HomeScreen extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // flexDirection:'row',
     padding: wp('5%'),
     backgroundColor: 'white',
+    
   },
   wrapContent: {
-    width: wp('90%'),
-    height: wp('90%'),
+    width: wp('45%'),
+    height: wp('80%'),
     paddingBottom: wp('5%'),
         
   },

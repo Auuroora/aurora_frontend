c73f896852d64f33d5132d9cd0bf72b76b996295
Jihwan Kim <noneobj@gmail.com>
2020-04-28 16:33:31 +0900

    Remove Context API

--- App.js
+++ App.js
@@ -8,8 +8,6 @@
 
 import React, { useEffect } from 'react'
 import AppNav from './src/Screens'
-import AuthProvider from './src/AuthProvider'
-
 import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
@@ -19,9 +17,7 @@ const App = () => {
   //TODO: Screens depth 줄이기, Components화 증가
   //TODO: Style Sheet -> 공통부분읜 Styles 폴더 따로, private한 부분은 component 폴더 안에 style로
   return (
-    <AuthProvider>
-      <AppNav/>
-    </AuthProvider>
+    <AppNav/>
   ) 
 }
 

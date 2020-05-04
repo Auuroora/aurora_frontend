4c38f297ce43e28390e5232d9d6f18b43a90c5da
Jihwan Kim <noneobj@gmail.com>
2020-05-04 21:05:45 +0900

    Fix font error

--- App.js
+++ App.js
@@ -11,9 +11,7 @@ import AppNav from './src/Screens'
 import SplashScreen from 'react-native-splash-screen'
 import { Provider } from 'react-redux'
 import configureStore from './src/Store/configureStore'
-import { Examples } from '@shoutem/ui';
 
-import { Examples } from '@shoutem/ui'
 const store = configureStore()
 
 const App = () => {
@@ -24,7 +22,7 @@ const App = () => {
   //TODO: Style Sheet -> 공통부분읜 Styles 폴더 따로, private한 부분은 component 폴더 안에 style로
   return (
     <Provider store={store}>
-      <Examples/>
+      <AppNav/>
     </Provider>
   ) 
 }

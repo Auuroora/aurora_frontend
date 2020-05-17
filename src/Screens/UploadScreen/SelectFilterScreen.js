3903632e23b2bca944be0f82b674eed8a861ba92
zzudy <wngus1100@gmail.com>
2020-05-17 21:00:55 +0900

    selectScreen api 연결

--- src/Screens/UploadScreen/SelectFilterScreen.js
+++ src/Screens/UploadScreen/SelectFilterScreen.js
@@ -7,53 +7,26 @@ import {
 } from '@shoutem/ui'
 import Tile from './Tile'
 import PropTypes from 'prop-types'
+import axios from '../../axiosConfig'
+
+const base_url ="https://aurora-filter-storage.s3.ap-northeast-2.amazonaws.com/"
 
 class SelectFilterScreen extends Component{
   constructor(props) {
     super(props)
     this.renderRow = this.renderRow.bind(this)
     this.state = {
-      item: [
-        {
-          "name": "Gaspar Brasserie",
-          "filterId": 1,
-          "price": "$150",
-          "image": { "url": "https://shoutem.github.io/static/getting-started/restaurant-1.jpg" },
-        },
-        {
-          "name": "Gaspar Brasserie",
-          "price": "$150",
-          "filterId": 2,
-          "image": { "url": "https://shoutem.github.io/img/ui-toolkit/examples/image-2.png"},
-        },
-        {
-          "name": "Gaspar Brasserie",
-          "price": "$150",
-          "filterId": 3,
-          "image": { "url": "https://shoutem.github.io/img/ui-toolkit/examples/image-11.png" },
-        },
-        {
-          "name": "Gaspar Brasserie",
-          "price": "$150",
-          "filterId": 4,
-          "image": { "url": "https://shoutem.github.io/img/ui-toolkit/examples/image-9.png" },
-        },
-        {
-          "name": "Gaspar Brasserie",
-          "price": "$150",
-          "filterId": 1,
-          "image": { "url": "https://shoutem.github.io/img/ui-toolkit/examples/image-2.png"},
-        }
-      ]
+      filter_list: []
     }
   }
   renderRow(rowData) {  
-    const cellViews = rowData.map((item, id) => {
+    const cellViews = rowData.map((filter, id) => {
+      // console.log(filter)
       return (
         <Tile 
           key={id}
-          image={item.image.url} 
-          filterId={item.filterId}
+          image={base_url + filter.filter_info.filter_name} 
+          filterId={filter.filter_info.filter_id}
           onPressDone ={this.props.onPressDone} 
         />
       )
@@ -66,11 +39,19 @@ class SelectFilterScreen extends Component{
   }
 
   render(){
-    const item = this.state.item
-    const groupedData = GridRow.groupByRows(item, 3, 
+    axios.get('/filters',{"user_info" : "true"}).then(res => {
+      const filterData = res.data
+      this.setState({filter_list:filterData}) 
+    })
+    
+    // res = await axios.post('/filters', data)
+    const filter_list = this.state.filter_list
+    // console.log(filter_list)
+    const groupedData = GridRow.groupByRows(filter_list, 3, 
       () => {
         return 1
       })
+    
     return (   
       <Screen styleName='fill-parent'>
         <ListView
